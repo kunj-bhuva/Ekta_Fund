423aace29ddb9a3935e39faf61c00a0dba038fb4
SakshiShah11 <202201281@daiict.ac.in>
2024-09-28 18:15:54 +0530

    updated home page

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Body.css';  // Import the CSS for the Header component 
 import loginImage from '../images/Mobile-login-Cristina 1.png';
+import RegistrationForm from './RegistrationForm';
 
 
 export default function Body() {
@@ -10,6 +11,7 @@ export default function Body() {
                 <h1 className="content-headline">Register Your <br/>Organization </h1>
                 <p> Fill out the form below to register your <br/> Organization with EktaFund</p>
                 <img src={loginImage} alt="loginImage" className="login-image" />
+                <RegistrationForm/>
             </div>
         </div>
     );
