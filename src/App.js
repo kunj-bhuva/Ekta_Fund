2e779b35aa3a79bfa8d67bdc089397abf944efca
kunj-bhuva <202201275@daiict.ac.in>
2024-10-20 02:15:03 +0530

    boot

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./..node_modeules/bootstrap/dist/css/bootstrap.min.css";
 import './App.css';  // Importing the App-specific CSS file
 import Header from './components/Header';  // Importing the Header component (from your example)
 import Body from './components/Body'; 
