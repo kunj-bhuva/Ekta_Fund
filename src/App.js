8c35493e6e0d0a23a07da77af3c0589125356f77
SakshiShah11 <202201281@daiict.ac.in>
2024-11-05 21:32:21 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS correctly
 import './App.css';  // Importing the App-specific CSS file
 import Header from './components/Header_white';  // Importing the Header component
-import Body from './components/Home';  // Importing the Body component
+// import Body from './components/Home';  // Importing the Body component
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Body />
+     
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './footer.css';
+import './Footer.css';
 
 export default function Footer(){
     <div className='container'>
