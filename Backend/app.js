bdcc41a63f92785905807fd618bccc5a7b85d749
202201315 <202201315@daiict.ac.in>
2024-11-14 17:28:58 +0530

    update

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,7 @@ const connectDB = require("./db/connectDB");
 const donorRoutes = require("./routes/donorRoutes");
 const ngoRoutes = require("./routes/ngoRoutes");
 const adminRoutes = require("./routes/adminRoutes");  
-const path = require("path"); 
+
 
 dotenv.config();
 connectDB();
@@ -18,9 +18,6 @@ app.use("/api/ngos", ngoRoutes);
 app.use("/api/admin", adminRoutes);
 
 // Catch-all route to serve the front-end index.html for any other request
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "index.html"));
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
