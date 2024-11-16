9ab13a4bec8a41420e1341d86521c816d5fa1c4f
Devamm Patel <202201292@daiict.ac.in>
2024-11-16 18:26:28 +0530

    Creating route for donation

---
+++ Backend/routes/donationRoutes.js
@@ -0,0 +1,9 @@
+// routes/donationRoutes.js
+const express = require("express");
+const router = express.Router();
+const donationController = require("../controllers/donationController");
+
+router.post("/donate", donationController.processDonation);
+
+
+module.exports = router;
