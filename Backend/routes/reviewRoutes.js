0178d92ffb9cc476fc839546ff603179cd16d6da
Ram Kulkarni <kulkarniram818@gmail.com>
2024-11-17 16:07:53 +0530

    reviewRoutes adding

---
+++ Backend/routes/reviewRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { addReview, addWebsiteReview } = require('../controllers/reviewController');
+const router = express.Router();
+
+// Route for donor reviews with NGO name
+router.post('/reviews', addReview);
+
+// Route for website reviews
+router.post('/website-reviews', addWebsiteReview);
+
+module.exports = router;
