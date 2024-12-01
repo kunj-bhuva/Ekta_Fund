f9344c66a21561bfaf70406eae5a55f863be1fc9
kunj-bhuva <202201275@daiict.ac.in>
2024-12-01 18:37:38 +0530

    okk

--- Backend/app.js
+++ Backend/app.js
@@ -32,11 +32,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
+  cors()
 );
 app.use(morgan("dev")); 
 app.use(express.static(path.join(__dirname, "public"))); 
